a207e79a8902679681383a93a6aeb41c6c4ecf74
isuru <isurutmv@gmail.com>
2018-12-10 00:51:18 +0530

    style the home page

--- index.php
+++ index.php
@@ -5,17 +5,23 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="assets/css/style.css">
     <title>Woogle-Search Enyrhing</title>
 </head>
 <body>
+
+<div class="wrapper indexPage">
    <div class="mainSection">
        <div class="logoContainer">
-           <img src="https://www.google.lk/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png" alt="">
+           <img src="assets/img/logo.png" alt="">
        </div>
        <div class="searchContainer">
+           <form method="get" action="search.php">
            <input type="text" class="searchBox" name="term" placeholder="Enter Search Term......">
            <input type="submit" class="searchButton" value="Search">
+           </form>
        </div>
    </div>
+</div>
 </body>
 </html>
